1146ebad09a3b0dea8f439841dbe841f025b9114
CaoVanThanh2k <94498311+CaoVanThanh2k@users.noreply.github.com>
2021-11-27 23:46:45 +0700

    Add files via upload

---
+++ SocketTong/src/sockettong/Server.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sockettong;
+
+/**
+ *
+ * @author Hieu Bom IT
+ */
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+ 
+public class Server {
+ 
+   public static void main(String args[]) {
+ 
+       ServerSocket cong = null;
+       String line="";
+       BufferedReader is;
+       BufferedWriter os;
+       Socket socket = null;
+       try {
+           cong = new ServerSocket(9999);
+       } catch (IOException e) {
+           System.out.println(e);
+           System.exit(1);
+       }
+       try {
+           System.out.println("Chạy Client!");
+           socket = cong.accept();
+           System.out.println("Chấp nhận máy con!");
+           is = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+           os = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+           //while (true) {
+               int a = Integer.parseInt(is.readLine());
+               int b = Integer.parseInt(is.readLine());
+               String c=String.valueOf(a+b);
+               os.write(c);
+               os.newLine();
+               os.flush();  
+           //}
+ 
+       } catch (IOException e) {
+           System.out.println(e);
+       }
+       System.out.println("Dừng kết nối Sever!");
+   }
+}
